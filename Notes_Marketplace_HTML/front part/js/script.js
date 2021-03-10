57044b03ebb3564028050add9665ab01fde499f6
Parth Patel <parthpatel9265@gmail.com>
2021-03-10 22:33:20 +0530

    Initial commit

---
+++ Notes_Marketplace_HTML/front part/js/script.js
@@ -0,0 +1,93 @@
+
+/* ==================================
+            toggle password
+==================================== */
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+/* ==================================
+           navigation
+====================================*/
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        showHideNav();
+
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            //Show White nav
+            $(".nav-scroll").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".nav-scroll .navbar-brand img").attr("src", "img/pre-login/logo.png");
+
+        } else {
+
+            // Hide White nav
+            $(".nav-scroll").removeClass("white-nav-top");
+
+            // Show logo
+            $(".nav-scroll .navbar-brand img").attr("src", "img/pre-login/top-logo.png");
+
+        }
+    }
+});
+
+/* ==================================
+            mobile menu 
+====================================*/
+$(function () {
+
+    // Show Mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide Mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav ").click(function () {
+        $("#mobile-nav").css("height", "0");
+    });
+
+
+});
+
+/* ==================================
+           faq
+====================================*/
+$(document).ready(function () {
+
+    for (let i = 1; i <= 7; i++) {
+        $(".showdata" + i).click(function () {
+            $(".ans" + i).show();
+            $(".que" + i).hide();
+        });
+        $(".hidedata" + i).click(function () {
+            $(".ans" + i).hide();
+            $(".que" + i).show();
+        });
+    }
+});
+
+
+
+
+
+
